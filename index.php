07848853be1ce9b389a1984b778573f6f959aa61
andyF845 <wonky@yandex.ru>
2014-06-09 08:44:23 +0400

    changed index.php (json+numeric error codes)

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 include 'sqlcon.php';
+include 'errors.php';
 
 try {
 	$sql = new MySQLcon ( '127.0.0.1', 'root', '', 'inventory' );
@@ -16,25 +17,25 @@ $act = $_GET ['act'];
 try {
 	switch ($act) {
 		case 'getAll' :
-			$res = $sql->getXMLResult ( "SELECT * FROM data WHERE 1;" );
+			$res = $sql->getJSONResult ( "SELECT * FROM data WHERE 1;" );
 			break;
 		case 'getProblems' :
-			$res = $sql->getXMLResult ( "SELECT * FROM data WHERE state<>0;" );
+			$res = $sql->getJSONResult ( "SELECT * FROM data WHERE state<>0;" );
 			break;
 		case 'getQR' :
-			$res = $sql->getXMLResult ( "SELECT * FROM data WHERE qr='$QR' LIMIT 1;" );
+			$res = $sql->getJSONResult ( "SELECT * FROM data WHERE qr='$QR' LIMIT 1;" );
 			break;
 		case 'set' :
-			if ($QR == '') throw new Exception('QR must be set');
-			if ($NAME == '') throw new Exception('Name must be set');
+			if (($QR   == '') || ($NAME == '')) throw new Exception(ERR_BAD_DATA);
 			$sql->goSQL ( "INSERT INTO data VALUES ('$QR','$NAME','$MEMO',$STATE) ON DUPLICATE KEY UPDATE qr=qr;" );
+			$res = ERR_OK;
 			break;
-		default: throw new Exception('Unknown command');	
+		default: throw new Exception(ERR_UNKNOWN_COMMAND);	
 	}
-	$res? (header('Content-type: application/xml') || (print $res)) : print 'ok';
+	echo empty($res)? ERR_NOT_FOUND : $res;
 	/*
 	 * debug 
-	 * echo "<form action=\"?act=set&qr=0123456789ABCDEF\" method=\"POST\"> QR:<br> <input name=\"qr\"><br> Name:<br> <input name=\"name\"><br> Memo:<br> <textarea name=\"memo\"></textarea><br> State:<br> <input name=\"state\"><br> <input type=\"submit\"> </form>";
+	  echo "<form action=\"?act=set&qr=0123456789ABCDEF\" method=\"POST\"> QR:<br> <input name=\"qr\"><br> Name:<br> <input name=\"name\"><br> Memo:<br> <textarea name=\"memo\"></textarea><br> State:<br> <input name=\"state\"><br> <input type=\"submit\"> </form>";
 	 */
 } catch ( Exception $e ) {
 	die ( $e->getMessage () );
