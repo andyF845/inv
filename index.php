3347d00f4400f2374fb5338c75a3462c1568810e
andyF845 <wonky@yandex.ru>
2014-06-09 20:31:08 +0300

    added JSON encode for error codes, fixed <state> field.

--- index.php
+++ index.php
@@ -5,14 +5,14 @@ include 'errors.php';
 try {
 	$sql = new MySQLcon ( '127.0.0.1', 'root', '', 'inventory' );
 } catch ( Exception $e ) {
-	die ( ERR_MYSQL_SERVER_ERROR );
+	die ( json_encode ( array ( "error"=>ERR_MYSQL_SERVER_ERROR ) ) );
 }
 
 $act = $_GET ['act'];
 (isSet ( $_GET ['qr'] )) && ($QR = $sql->escapeString ( $_GET ['qr'] ));
 (isSet ( $_POST ['memo'] )) && ($MEMO = $sql->escapeString ( $_POST ['memo'] ));
 (isSet ( $_POST ['name'] )) && ($NAME = $sql->escapeString ( $_POST ['name'] ));
-((is_numeric ( $_GET ['state'] )) && ($STATE = $_GET ['state'])) || ($STATE = 0);
+((is_numeric ( $_POST ['state'] )) && ($STATE = $_POST ['state'])) || ($STATE = 0);
 
 try {
 	switch ($act) {
@@ -32,8 +32,9 @@ try {
 			break;
 		default: throw new Exception(ERR_UNKNOWN_COMMAND);	
 	}
-	echo empty($res)? ERR_NOT_FOUND : $res;
+	if (empty($res)) throw new Exception(ERR_NOT_FOUND);
+	echo $res;
 } catch ( Exception $e ) {
-	die ( $e->getMessage () );
+	die ( json_encode ( array ( "error"=>$e->getMessage() ) ) );
 }
 ?>
