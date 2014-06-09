f0fc854d43e1cf909f560de145320734ef63ae44
andyF845 <wonky@yandex.ru>
2014-06-09 08:38:17 +0400

    Added errors.php

---
+++ errors.php
@@ -0,0 +1,12 @@
+<?php
+//OK
+define(ERR_OK,10);
+//Get errors 11..19
+define(ERR_NOT_FOUND,11);
+//Set errors 21..29
+define(ERR_BAD_DATA,21);
+//MySQL errors 31..39
+define(ERR_MYSQL_SERVER_ERROR,31);
+//Unknown command
+define(ERR_UNKNOWN_COMMAND,40);
+?>
